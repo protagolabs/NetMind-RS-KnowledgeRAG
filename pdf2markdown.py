f61f7e3355fada73719cfc71afb07611bd2fd7e5
xinghuyou <1294025680@qq.com>
2025-07-31 10:56:28 +0800

    add year reports data

--- pdf2markdown.py
+++ pdf2markdown.py
@@ -6,19 +6,15 @@ url = "https://api.netmind.ai/inference-api/agent/v1/parse-pdf"
 
 url2="https://github.com/protagolabs/NetMind-RS-KnowledgeRAG/raw/GuoCheng/pdf_to_markdown/pdf/"
 
-# payload = json.dumps({
-#    "url": "https://github.com/protagolabs/NetMind-RS-KnowledgeRAG/blob/GuoCheng/pdf_to_markdown/pdf/2010.11929v2.pdf",
-#    "format": "markdown",
-#    "vlm": True
-# })
 headers = {
    'Authorization': 'Bearer ffcba38d4999436a986939e216a61a9f',
    'Content-Type': 'application/json'
 }
 
 for file in os.listdir("pdf_to_markdown/pdf"):
+    
     file=file.replace(" ","%20")
-    # print(file)
+   #  print(file)
     new_url=url2+file
     # print(new_url)
     payload = json.dumps({
@@ -29,5 +25,6 @@ for file in os.listdir("pdf_to_markdown/pdf"):
     file=file.replace("%20"," ")
     file=file.replace(".pdf",".md")
     response = requests.request("POST", url, headers=headers, data=payload)
-    with open("pdf_to_markdown/markdown/"+file, "w", encoding="utf-8") as f:
-        f.write(response.text)
+
+    # with open("pdf_to_markdown/markdown/"+file, "w", encoding="utf-8") as f:
+    #     f.write(response.text)
