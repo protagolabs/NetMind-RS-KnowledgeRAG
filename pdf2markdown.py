5e7c3671c2aafcc0351708e46e696e4a8d58f689
xinghuyou <1294025680@qq.com>
2025-08-19 04:17:43 -0400

    rename insurance folder name

--- pdf2markdown.py
+++ pdf2markdown.py
@@ -4,7 +4,7 @@ import os
 
 url = "https://api.netmind.ai/inference-api/agent/v1/parse-pdf"
 
-url2="https://github.com/protagolabs/NetMind-RS-KnowledgeRAG/raw/GuoCheng/pdf_to_markdown/insurance_pdf/"
+url2="https://github.com/protagolabs/NetMind-RS-KnowledgeRAG/raw/GuoCheng/pdf_to_markdown/%20insurance_pdf/"
 
 headers = {
    'Authorization': 'Bearer ffcba38d4999436a986939e216a61a9f',
@@ -14,17 +14,17 @@ headers = {
 for file in os.listdir("pdf_to_markdown/insurance_pdf"):
     
     file=file.replace(" ","%20")
-    # print(file)
-    new_url=url2+file
-   #  print(new_url)
-    payload = json.dumps({
-   "url":new_url,
-   "format": "markdown",
-   "vlm": True
-    })
-    file=file.replace("%20"," ")
-    file=file.replace(".pdf",".md")
-    response = requests.request("POST", url, headers=headers, data=payload)
+    print(file)
+#     new_url=url2+file
+#    #  print(new_url)
+#     payload = json.dumps({
+#    "url":new_url,
+#    "format": "markdown",
+#    "vlm": True
+#     })
+#     file=file.replace("%20"," ")
+#     file=file.replace(".pdf",".md")
+#     response = requests.request("POST", url, headers=headers, data=payload)
 
-    with open("pdf_to_markdown/insurance_pdf_markdown/"+file, "w", encoding="utf-8") as f:
-        f.write(response.text)
+#     with open("pdf_to_markdown/insurance_pdf_markdown/"+file, "w", encoding="utf-8") as f:
+#         f.write(response.text)
